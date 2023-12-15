e9d5a50f1d8b3d3388ad2cb1ef30fee4c04ecf97
Naveen <naveenmc777@gmail.com>
2023-12-15 19:10:09 +0530

    first commit in visual code

--- mc.py
+++ mc.py
@@ -1,7 +1,1 @@
-<<<<<<< HEAD
-This is thr first line
-this is second line commit
-=======
-This is the first line
-Making sure it is updating 
->>>>>>> 845f045e581902df0ea0a4e17c1f89f7ef4a6112
+Hi this is new method of git via visual code
